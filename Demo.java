4302a0750215df5b6375bbf246759f3e8ba64269
Anuj <suryawanshianuj002@gmail.com>
2026-08-06 14:12:06 +0530

    Jira 101 commit

--- Demo.java
+++ Demo.java
@@ -1,10 +1,14 @@
 public class Demo {
-
-
     //JIRA 102
     public void m2(){
         System.out.println("method 2 JIRA Story 102");
     }
+    //JIRA Story - 101
+    public void m1(){
+        System.out.println("method 1");
+    }
+    
+>>>>>>> Stashed changes
     public static void main(String[] args) {
         int a=12;
         int b=13;
