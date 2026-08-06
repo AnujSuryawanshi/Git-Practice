30413f757409855db230d9741f7d9494617eb3be
Anuj <suryawanshianuj002@gmail.com>
2026-08-06 14:04:53 +0530

    Jira 102 commit

--- Demo.java
+++ Demo.java
@@ -1,4 +1,10 @@
 public class Demo {
+
+
+    //JIRA 102
+    public void m2(){
+        System.out.println("method 2 JIRA Story 102");
+    }
     public static void main(String[] args) {
         int a=12;
         int b=13;
